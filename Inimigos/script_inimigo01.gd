f6420fb43b42f1de91e713f97a5cfc75febab0af
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-05-01 19:56:53 -0300

    Inimigo01 finished

--- Inimigos/script_inimigo01.gd
+++ Inimigos/script_inimigo01.gd
@@ -3,28 +3,42 @@ extends KinematicBody2D
 export var velocidade = 64
 export var vida = 2
 var movimentacao = Vector2.ZERO
-var direcao_movimento = -1
+var direcao_movimento = 1
 var gravidade = 1200
 var atingido = false
 
+export var movimentacao_rayCast = false
 export var esquerda_maxima = 800
 export var direita_maxima = 1000
 
+func _ready():
+	if(!movimentacao_rayCast):
+		$RayCast2D.enabled = false
+
 func _physics_process(delta):	
+	movimentacao.y += gravidade * delta 
+	
 	if(!atingido):
 		movimentacao.x = velocidade * direcao_movimento
-		movimentacao.y += gravidade * delta 
 		
 		if(direcao_movimento == -1):
 			$Sprite.flip_h = true
 		else:
 			$Sprite.flip_h = false
+		
+		if(movimentacao_rayCast):
+			if(!$RayCast2D.is_colliding()):
+				$AnimationPlayer.play("parado")
+				movimentacao.x = 0
+		else:
+			if($".".global_position.x <= esquerda_maxima && direcao_movimento == -1):
+				$AnimationPlayer.play("parado")
+				movimentacao.x = 0
+				
+			if($".".global_position.x >= direita_maxima && direcao_movimento == 1):
+				$AnimationPlayer.play("parado")
+				movimentacao.x = 0
 			
-		if($".".global_position.x <= esquerda_maxima && direcao_movimento == -1):
-			$AnimationPlayer.play("parado")
-			
-		if($".".global_position.x >= direita_maxima && direcao_movimento == 1):
-			$AnimationPlayer.play("parado")
 	else:
 		movimentacao.x = 0
 		
@@ -33,6 +47,8 @@ func _physics_process(delta):
 func _animacao_personagem_terminou(anim_name):
 	if(anim_name == 'parado'):
 		$Sprite.flip_h != $Sprite.flip_h
+		if(movimentacao_rayCast):
+			$RayCast2D.position.x *= -1
 		direcao_movimento *= -1
 		$AnimationPlayer.play("andando")
 
@@ -46,7 +62,6 @@ func colidiu_com_inimigo(area):
 	
 	if(vida <= 0):
 		$".".set_collision_layer(0)
-		$CollisionShape2D.disabled = true
 		$CaixaAtaque.queue_free()
 		$AudioStreamPlayer2.play()
 		$AnimationPlayer.play("morrendo")
