791629b858a1ef40f0b80689e394dd4942e2803c
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-05-20 21:55:18 -0300

    Sound of the box breaking and sound of the box being crushed added

--- itens/script_caixa01.gd
+++ itens/script_caixa01.gd
@@ -3,9 +3,15 @@ extends KinematicBody2D
 export var pontos = 20
 
 func personagem_pisou(body: Node) -> void:
-	print(body.name)
 	if(body.name == "AtaqueMarreta" || body.name == "Spinner"):
+		$AudioStreamPlayer2.play()
+		$CaixaAtaque1.queue_free()
+		$CaixaAtaque2.queue_free()
+		$Sprite.visible = false
+		$CollisionShape2D.set_deferred("disabled", true)
+		yield(get_tree().create_timer(0.15), "timeout")
 		queue_free()
 	elif body.name == "Personagem":
 		$AnimationPlayer.play("pisada")
+		$AudioStreamPlayer.play()
 		body.movimentacao.y -= 600
