58e91a0dacfbf19342fda2504e4a202ad2dfc691
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-05-29 21:33:05 -0300

    Added option to select game difficulty

--- telas/script_cena_opcoes.gd
+++ telas/script_cena_opcoes.gd
@@ -3,6 +3,12 @@ extends Node2D
 func _ready():
 	$Node2D/SldMusica.value = ScriptGlobal.musicaVolume
 	$Node2D/SldSons.value = ScriptGlobal.sonsVolume
+	
+	$Node2D/OptnDificuldade.add_item("Fácil")
+	$Node2D/OptnDificuldade.add_item("Médio")
+	$Node2D/OptnDificuldade.add_item("Difícil")		
+	
+	$Node2D/OptnDificuldade.select(ScriptGlobal.DificuldadeJogo - 1)
 
 func _process(_delta):
 	ScriptGlobal.musicaVolume = $Node2D/SldMusica.value
@@ -13,3 +19,7 @@ func botao_voltar_cena_autenticado():
 		get_tree().change_scene("res://telas/cena_inicial.tscn")
 	else:
 		get_tree().change_scene("res://telas/cena_autenticado.tscn")
+
+
+func nivel_dificuldade_selecionado(index):
+	ScriptGlobal.DificuldadeJogo = index + 1
