e7ddfdacc2e21dab38d22106380b7e2ebe81a011
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-05-30 14:17:50 -0300

    Login screen configured

--- scripts/script_global.gd
+++ scripts/script_global.gd
@@ -6,16 +6,23 @@ var spinner_mov = Vector2(10, 0.5)
 func _ready():
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, minimumSize, 1)
 
+#Opções
 var musicaVolume = -5
 var sonsVolume = -5
-
+var DificuldadeJogo = 1
 var telaOrigemAteOpcoes = ""
 
+#Controle Fase
 var faseAtual = 1
-
 var QuantidadeSpinner = 0
 var QuantidadeVida = 0
 var QuantidadePontos = 0
 var TempoDaFase = 120
 var PersonagemMorreu = false
-var DificuldadeJogo = 1
+
+#Back-end
+var BdURL = "http://olhosdesafiantes.eu5.org"
+
+#Jogador
+var IdJogador = 0
+var NomeJogador = ""
--- telas/script_cena_entrar.gd
+++ telas/script_cena_entrar.gd
@@ -1,7 +1,38 @@
 extends Node2D
 
-func botao_entrar():
-	get_tree().change_scene("res://telas/cena_autenticado.tscn")
-
 func botao_voltar_para_tela_inicial():
 	get_tree().change_scene("res://telas/cena_inicial.tscn")
+
+func botao_entrar():
+	var url_requisicao = ScriptGlobal.BdURL + "/servico/verificar_se_cadastrado.php"
+	var dados_envio = "email="+$Node2D/LiEdEmail.text
+	var cabecalho   = ["Content-Type: application/x-www-form-urlencoded"] 
+	$Node2D/HTTPRequest.request(url_requisicao, cabecalho, false,HTTPClient.METHOD_POST, dados_envio)
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	if (json.result.size()>0):
+		var url_requisicao = ScriptGlobal.BdURL + "/servico/entrar.php"
+		var dados_envio = "email="+$Node2D/LiEdEmail.text+"&senha="+$Node2D/LiEdSenha.text
+		var cabecalho   = ["Content-Type: application/x-www-form-urlencoded"] 
+		$Node2D/HTTPRequest2.request(url_requisicao, cabecalho, false,HTTPClient.METHOD_POST, dados_envio)
+	else:
+		$CanvasLayer/Control/AcceptDialog.dialog_text = "Email não cadastrado!";
+		$CanvasLayer/Control/AcceptDialog.show();
+		
+		$Node2D/RichTextLabel.visible = true
+		$Node2D/RichTextLabel2.visible = true
+
+
+func _on_HTTPRequest2_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	if (json.result.size()>0):
+		ScriptGlobal.IdJogador = json.result["id"]
+		ScriptGlobal.NomeJogador = json.result["nome"]
+		var status =  json.result["status"]
+		
+		get_tree().change_scene("res://telas/cena_autenticado.tscn")
+	else:
+		$CanvasLayer/Control/AcceptDialog.dialog_text = "Email ou Senha inválidos!";
+		$CanvasLayer/Control/AcceptDialog.show();
