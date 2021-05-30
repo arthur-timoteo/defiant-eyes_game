6f674f71de6a83f4505a61e8b9d8fb47cea6f615
Arthur Timoteo <arthur.timoteo@gmail.com>
2021-05-30 19:56:58 -0300

    Adjustments in the metrics screen table

--- telas/script_cena_desempenho.gd
+++ telas/script_cena_desempenho.gd
@@ -7,22 +7,33 @@ func _ready():
 func _on_HTTPRequest_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
 	
+	$Node2D/ItemList.add_item("N° da fase")
+	$Node2D/ItemList.add_item("Tempo gasto")
+	$Node2D/ItemList.add_item("Pontos")
+	$Node2D/ItemList.add_item("Quantidade de vidas")
+	$Node2D/ItemList.add_item("Inimigos matados")
+	$Node2D/ItemList.add_item("Data")
+	
 	for i in range(json.result.size()):
-		$Node2D/ItemList.add_item(json.result[i].numero_fase + " - " + json.result[i].tempo_gasto + " - " + json.result[i].pontos + " - " + json.result[i].quantidade_vidas + " - " + json.result[i].inimigos_matados + " - " + json.result[i].data)
-		
+		$Node2D/ItemList.add_item(json.result[i].numero_fase)
+		$Node2D/ItemList.add_item(json.result[i].tempo_gasto)
+		$Node2D/ItemList.add_item(json.result[i].pontos)
+		$Node2D/ItemList.add_item(json.result[i].quantidade_vidas)
+		$Node2D/ItemList.add_item(json.result[i].inimigos_matados)
+		$Node2D/ItemList.add_item(json.result[i].data)
 
 func _on_HTTPRequest2_request_completed(result, response_code, headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
 	
 	for i in range(json.result.size()):
 		if(json.result[i].id == ScriptGlobal.IdJogador):
-			$Node2D/RichTextLabel.text = json.result[i].nome + " confira seu desempenho no jogo"
+			$Node2D/RichTextLabel.text = json.result[i].nome + ","
 			$Node2D/RichTextLabel2.text = "Pontos: " + str(json.result[i].pontos)
 			$Node2D/RichTextLabel3.text = "Posição no ranking: " + str(i + 1)
 	
 func buscar_lista_desempenho():
 	var url_requisicao = ScriptGlobal.BdURL + "/servico/desempenho_jogador.php?"
-	var dados_envio = "id="+str(ScriptGlobal.IdJogador)
+	var dados_envio = "formato_data=d/m/Y&id="+str(ScriptGlobal.IdJogador)
 	var cabecalho   = ["Content-Type: application/json"]
 	$Node2D/HTTPRequest.request(url_requisicao + dados_envio, cabecalho, false, HTTPClient.METHOD_GET)
 	
